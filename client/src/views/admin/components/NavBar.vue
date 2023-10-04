f070b489f219167d21870aba3037e9442e56eb4e
ogzCode <oguzguc44@gmail.com>
2023-10-04 12:01:09 +0300

    Added active routing class and added active order count

--- client/src/views/admin/components/NavBar.vue
+++ client/src/views/admin/components/NavBar.vue
@@ -3,13 +3,24 @@
         <div class="flex items-center gap-20">
             <h1 class="text-4xl font-bold tracking-wide text-slate-800">Food<span class="text-orange-600">-ER</span></h1>
             <nav>
-                <router-link :to="{ name: 'Orders' }" class="mx-8 font-medium tracking-wide text-slate-800 hover:text-orange-600 transition">Orders</router-link>
-                <router-link :to="{ name: 'Products' }" class="mx-8 font-medium tracking-wide text-slate-800 hover:text-orange-600 transition">Products</router-link>
-                <router-link :to="{ name: 'Users' }" class="mx-8 font-medium tracking-wide text-slate-800 hover:text-orange-600 transition">Users</router-link>
+                <router-link :to="{ name: 'Orders' }"
+                    class="mx-8 font-medium tracking-wide hover:text-orange-600 transition"
+                    :class="[isRouteActive('Orders')]">Orders</router-link>
+                <router-link :to="{ name: 'Products' }"
+                    class="mx-8 font-medium tracking-wide hover:text-orange-600 transition"
+                    :class="[isRouteActive('Products')]">Products</router-link>
+                <router-link :to="{ name: 'Users' }"
+                    class="mx-8 font-medium tracking-wide hover:text-orange-600 transition"
+                    :class="[isRouteActive('Users')]">Users</router-link>
             </nav>
         </div>
-        <div>
-            <a class="font-medium text-sm tracking-wide mx-8 px-6 py-2 rounded border border-red-600 text-red-600 flex items-center logout-btn" href="#">
+        <div class="flex items-center">
+            <div class="border border-orange-600 text-orange-600 font-medium text-xs px-4 py-2 rounded-full tracking-wide relative">
+                Orders
+                <span class="absolute -top-2 -right-2 text-white bg-orange-600 rounded-full flex justify-center items-center text-xs w-6 h-6 ">2</span>
+            </div>
+            <a class="font-medium text-sm tracking-wide mx-8 px-6 py-2 rounded border border-red-600 text-red-600 flex items-center logout-btn"
+                href="#">
                 <span>Logout</span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
                     class="bi bi-box-arrow-right relative left-2" viewBox="0 0 16 16">
@@ -23,14 +34,26 @@
 </template>
 
 <script setup>
+import { useRoute } from 'vue-router';
+
+const $route = useRoute();
+
+const isRouteActive = (routeName) => {
+    return routeName === $route.name ? "text-orange-600" : "text-slate-800";
+}
+
 </script>
 
 <style scoped>
-
 .logout-btn svg {
     transition: all 0.3s ease-in-out;
 }
+
 .logout-btn:hover svg {
     left: 16px;
+}
+
+.active-link {
+    color: #F97316;
 }
 </style>
