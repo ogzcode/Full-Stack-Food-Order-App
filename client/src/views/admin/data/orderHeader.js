4c843f0c49e67ea1b6046944785a7d009765b7a7
ogzCode <oguzguc44@gmail.com>
2023-10-04 12:05:49 +0300

    Fake data and table headers were written for Users and Products

---
+++ client/src/views/admin/data/orderHeader.js
@@ -0,0 +1,46 @@
+export const orderHeader = [
+    {
+        columnName: "Order No",
+        key: "no",
+        width: "80px",
+        sortEnabled: true,
+    },
+    {
+        columnName: "Name",
+        key: "name",
+        width: "120px",
+        sortEnabled: true,
+    },
+    {
+        columnName: "Phone",
+        key: "phone",
+        width: "120px",
+    },
+    {
+        columnName: "Address",
+        key: "address",
+        width: "160px",
+    },
+    {
+        columnName: "Date",
+        key: "createTime",
+        width: "120px", 
+        sortEnabled: true,
+    },
+    {
+        columnName: "Price",
+        key: "total",
+        width: "60px",
+    },
+    {
+        columnName: "Status",
+        key: "status",
+        width: "120px",
+        sortEnabled: true,
+    },
+    {
+        columnName: "Actions",
+        key: "actions",
+        width: "160px",
+    }
+];
---
+++ client/src/views/admin/data/userHeader.js
@@ -0,0 +1,35 @@
+export const userHeader = [
+    {
+        columnName: "Name",
+        key: "name",
+        width: "120px",
+        sortEnabled: true,
+    },
+    {
+        columnName: "Email",
+        key: "email",
+        width: "120px",
+        sortEnabled: true,
+    },
+    {
+        columnName: "Phone",
+        key: "phone",
+        width: "120px",
+    },
+    {
+        columnName: "Order Count",
+        key: "orderCount",
+        width: "120px",
+    },
+    {
+        columnName: "Created At",
+        key: "createTime",
+        width: "120px", 
+        sortEnabled: true,
+    },
+    {
+        columnName: "Actions",
+        key: "actions",
+        width: "120px",
+    }
+];
